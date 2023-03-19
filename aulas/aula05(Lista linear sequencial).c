0b65f8b5b8d50b30d271e2f67ada5095e5a9285d
Victor Coelho <coelhovictor0715@gmail.com>
2023-03-19 17:46:30 -0300

    add aula05

--- aulas/aula05(Lista linear sequencial).c
+++ aulas/aula05(Lista linear sequencial).c
@@ -5,12 +5,45 @@ typedef int TIPOCHAVE;
 typedef struct{
 
 TIPOCHAVE chave; // dando um nome, TIPOCHAVE, pro tipo int
-// outros campos
+// outros campos...
 
 } REGISTRO;
 
 typedef struct {
-        REGISTRO A[MAX];
+    REGISTRO A[MAX];
+    int nroElem;
 
 } LISTA;
 
+
+
+// INICIALIZAÇÃO :
+
+void inicializarLista(LISTA l){
+    l.nroElem = 0;
+}
+
+// RETORNAR O NUM DE ELEMENTOS:
+
+// basta retornar o valor do campo nroElem;
+
+int tamnho(LISTA* l){
+    return l->nroElem;
+}
+
+
+
+
+
+
+
+/*
+FUNÇÕES DE GERENCIAMENTO:
+
+Inicializar a estrutura;
+Retornar a quantidade de elememtos válidos;
+Exibir os elementos da estrutura;
+Buscar por um elemento na estrutura;
+Inserir elementos na estrutura;
+Reiniciar a estrutura;
+*/
